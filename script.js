cecd44dc3a6b940917110f06c9fa8a7b6905a320
yusufwahab <yusufabdulwhab@gmail.com>
2025-06-14 10:50:31 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// const sidebarHamburger = document.getElementById("hamburger-side");
+// const mainHamburger = document.getElementById("hamburger-main");
+// const leftSidebar = document.querySelector(".sidebar-left");
+// const mainSidebar = document.querySelector(".main")
+// const overallContainer = document.querySelector(".overall-container")
+// sidebarHamburger.addEventListener("click", function () {
+//     overallContainer.classList.remove("showed-main");
+//     overallContainer.classList.add("remove-sidebar")
+   
+// })
